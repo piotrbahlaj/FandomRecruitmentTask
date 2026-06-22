5645468ece347bca1c102fa341cfb8850f3775a7
Piotr Bahłaj <piotr.bahlaj@o2.pl>
2026-06-22 03:36:32 +0200

    Fixed typo in main wrapper section.

--- shared/src/commonMain/kotlin/fandom/piotrbahlaj/project/core/utilities/Constants.kt
+++ shared/src/commonMain/kotlin/fandom/piotrbahlaj/project/core/utilities/Constants.kt
@@ -2,7 +2,8 @@ package fandom.piotrbahlaj.project.core.utilities
 
 object Constants {
     const val TrendingArticlesEndpoint: String = "mobile-sidekick/trending/articles"
-    const val Title: String = "Title"
+    
+    const val Titles: String = "Titles"
     const val Images: String = "Images"
     const val Loading: String = "Loading..."
     const val Error: String = "Error"
